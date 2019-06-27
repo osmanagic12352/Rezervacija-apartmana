265411f5843ea8918ea8b036393390b4a6aa1ffc
Ervin Osmanagić <51996226+osmanagic12352@users.noreply.github.com>
2019-06-27 16:32:56 +0200

    Delete baza_rezervacija.py

--- baza_rezervacija.py
+++
@@ -1,9 +0,0 @@
-from sqlite3 import *
-
-kon = connect("baza_ap.db")
-
-c = kon.cursor()
-
-c.execute("""CREATE TABLE tabla (ime_i_prezime TEXT, self.mjesto TEXT, self.naziv_apartmana TEXT, datum_boravka TEXT, broj_osoba INTEGER)""")
-
-kon.close()
